e7c1e7eba1f67933c0922aa0832344fd5c96da51
Steven Bai <51672429+stevenbai0724@users.noreply.github.com>
2020-12-19 09:13:01 -0500

    Add files via upload

---
+++ brick.cpp
@@ -0,0 +1,14 @@
+//https://atcoder.jp/contests/abc186/tasks/abc186_a
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>roomCords;
+
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n, k;
+    cin>>n>>k;
+    cout<<n/k;
+
+    return 0;
+}
